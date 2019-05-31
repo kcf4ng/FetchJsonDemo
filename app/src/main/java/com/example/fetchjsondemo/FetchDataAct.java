ccae0243f7a47a6967fff644a32cefa6966354e2
kcfang <kcf4ng@gmail.com>
2019-05-31 19:12:47 +0800

    parse jsonobject and jsonarray

--- app/src/main/java/com/example/fetchjsondemo/FetchDataAct.java
+++ app/src/main/java/com/example/fetchjsondemo/FetchDataAct.java
@@ -1,6 +1,13 @@
 package com.example.fetchjsondemo;
 
 import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,9 +18,14 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class FetchDataAct extends AsyncTask<Void,Void,Void> {
-
+    String strKey ="MyLog";
     String strJson;
+    String strDataParse, strSingleParse;
+    Integer intTotal;
 
+
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected Void doInBackground(Void... voids) {
         try {
@@ -23,7 +35,6 @@ public class FetchDataAct extends AsyncTask<Void,Void,Void> {
             URLConnection conn = url.openConnection();
             //取得串流
             InputStream streamIn = conn.getInputStream();
-
             //準備開始解碼，首先，把剛剛的串流讀進來，製作一個串流讀取器(BufferReader)
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(streamIn));
             //做一個StringBuilder,接著不斷地去讀取串流，讀到他是NULL為止，在這之前則把每一行 append  到 StringBuilder 裡面
@@ -36,10 +47,30 @@ public class FetchDataAct extends AsyncTask<Void,Void,Void> {
             }
 
             strJson = html.toString();
-            
+
+            JSONObject jsonObject = new JSONObject(strJson);
+
+//          JSONArray ja = (JSONArray) jsonObject.opt("data");
+            JSONArray ja  = jsonObject.getJSONArray("data");
+
+           for(int i = 0 ; i<ja.length();i++){
+                JSONObject dataJo = ja.getJSONObject(i);
+                strDataParse = strDataParse+ dataJo.getString("mom_name");
+                Log.d(strKey,strDataParse);
+            }
+
+//           intTotal= (Integer) jsonObject.opt("total");
+//            strDataParse = intTotal.toString();
+
+
+
+
+
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
@@ -50,6 +81,7 @@ public class FetchDataAct extends AsyncTask<Void,Void,Void> {
     @Override
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
-        MainActivity.lblJson.setText(this.strJson);
+//        MainActivity.lblJson.setText(this.strJson);
+        MainActivity.lblJson.setText(strDataParse);
     }
 }
